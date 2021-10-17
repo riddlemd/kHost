eaf2289e2d0696d75fe348274beedf3398533377
Michael Riddle <riddlemd@gmail.com>
2021-10-17 13:53:28 -0500

    Added InMemoryDatabase EF support

---
+++ KHost.Common/Repositories/EF/InMemoryEFRepository.cs
@@ -0,0 +1,26 @@
+﻿using KHost.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHost.Common.Repositories.EF
+{
+    public class InMemoryEFRepository<TModel> : BaseEFRepository<TModel>
+        where TModel : class, IModelWithId
+    {
+        private static KHostDbContext GetDbContext()
+        {
+            var builder = new DbContextOptionsBuilder<KHostDbContext>().UseInMemoryDatabase(databaseName: "InMemory");
+
+            return new KHostDbContext(builder.Options);
+        }
+
+        public InMemoryEFRepository() : base(GetDbContext())
+        {
+
+        }
+    }
+}
