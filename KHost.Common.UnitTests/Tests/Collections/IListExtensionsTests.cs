28098084caf3ebdd58a1034e4d8e8c89dfeb16be
Michael Riddle <riddlemd@gmail.com>
2021-10-22 12:03:02 -0500

    Added some UnitTests for Common

---
+++ KHost.Common.UnitTests/Tests/Collections/IListExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using KHost.Common.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Categories;
+
+namespace KHost.Common.UnitTests.Tests.Collections
+{
+    public class IListExtensionsTests
+    {
+        private List<string> CreateListOfStrings() => new()
+        {
+            "John",
+            "David",
+            "Eric",
+            "Matt",
+            "Michael"
+        };
+
+        [Fact]
+        [Category(TestCategory.Success)]
+        public void ShouldMoveWhenProvidedOldIndexAndNewIndex()
+        {
+            // Given
+            var list = CreateListOfStrings();
+
+            var name = "Matt";
+
+            var oldIndex = list.IndexOf(name);
+
+            var newIndex = 2;
+
+            // When
+            list.Move(oldIndex, newIndex);
+
+            // Then
+            Assert.True(list[newIndex] == name);
+        }
+
+        [Fact]
+        [Category(TestCategory.Success)]
+        public void ShouldMoveToFirstWhenProvidedOldIndex()
+        {
+            // Given
+            var list = CreateListOfStrings();
+
+            var name = "Matt";
+
+            var oldIndex = list.IndexOf(name);
+
+            // When
+            list.MoveToFirst(oldIndex);
+
+            // Then
+            Assert.True(list.First() == name);
+        }
+
+        [Fact]
+        [Category(TestCategory.Success)]
+        public void ShouldMoveToLastWhenProvidedOldIndex()
+        {
+            // Given
+            var list = CreateListOfStrings();
+
+            var name = "Matt";
+
+            var oldIndex = list.IndexOf(name);
+
+            // When
+            list.MoveToLast(oldIndex);
+
+            // Then
+            Assert.True(list.Last() == name);
+        }
+    }
+}
---
+++ KHost.Common.UnitTests/Tests/Routing/DashedParameterTransformerTests.cs
@@ -0,0 +1,28 @@
+﻿using KHost.Common.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KHost.Common.UnitTests.Tests.Routing
+{
+    public class DashedParameterTransformerTests
+    {
+        [Fact]
+        public void ShouldTransformOutboundWhenProvidedObject()
+        {
+            // Given
+            var pascalName = "FindUserById";
+
+            var transformer = new DashedParameterTransformer();
+
+            // When
+            var dashedName = transformer.TransformOutbound(pascalName);
+
+            // Then
+            Assert.True(dashedName == "find-user-by-id");
+        }
+    }
+}
