014ccf723763f71d413a75ae9708363c2f8bc4f8
Michael Riddle <riddlemd@gmail.com>
2021-10-30 11:29:39 -0500

    Reworked Plugin logic to better group assemblies from same plugin.

---
+++ KHost.Common/Plugins/BasePlugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHost.Common.Plugins
+{
+    public class BasePlugin : IPlugin
+    {
+        public string Path { get; }
+
+        public PluginSettings Settings { get; }
+
+        public List<Assembly> Assemblies { get; } = new();
+
+        public BasePlugin(PluginSettings settings, string path, IEnumerable<Assembly>? assemblies = null)
+        {
+            Settings = settings;
+            Path = path;
+
+            if(assemblies != null)
+                Assemblies.AddRange(assemblies);
+        }
+
+        public IPlugin RegisterAssembly(Assembly assembly)
+        {
+            Assemblies.Add(assembly);
+
+            return this;
+        }
+
+        public IEnumerable<Assembly> GetAssemblies() => Assemblies.ToArray();
+    }
+}
---
+++ KHost.Common/Plugins/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace KHost.Common.Plugins
+{
+    public interface IPlugin
+    {
+        string Path { get; }
+
+        IPlugin RegisterAssembly(Assembly assembly);
+
+        IEnumerable<Assembly> GetAssemblies();
+    }
+}
